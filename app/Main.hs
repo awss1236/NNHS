7eeaa79c173564bd5a335900e0daf8c2b66550ef
awss1236 <awsssayah@gmail.com>
2023-09-15 09:49:05 +0100

    Refactored NN and added feedForward function.

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,19 @@ import System.Random
 
 type Node  = ([Float], Float)
 type Layer = [Node]
+type NN    = [Layer]
 
-validateNN :: [Layer] -> Bool
+validateNN :: NN -> Bool
 validateNN (l1:l2:ls) = let n = map (length.fst) l2 in if allSame n then length l1 == head n && validateNN (l2:ls) else False
                       where allSame (x1:x2:xs) = if x1 == x2 then allSame (x2:xs) else False
                             allSame _ = True
 validateNN _ = True
 
-createNN :: StdGen -> [Int] -> ([Layer], StdGen)
+randomSeq :: Random a => StdGen -> Int -> ([a], StdGen)
+randomSeq g 0 = ([], g)
+randomSeq g n = let (r, g') = random g in (\(a, b) -> (r:a, b)) $ randomSeq g' (n-1)
+
+createNN :: StdGen -> [Int] -> (NN, StdGen)
 createNN = createNN' 1
          where createNN' :: Int -> StdGen -> [Int] -> ([Layer], StdGen)
                createNN' p g (x:xs) = let (l, g') = foldr (\_ (ns, cg) -> let (n, ng) = randNode cg p in (n:ns, ng)) ([], g) [1..x]
@@ -21,9 +26,9 @@ createNN = createNN' 1
                                   (b, g'') = random g'
                               in ((ws, b), g'')
 
-randomSeq :: Random a => StdGen -> Int -> ([a], StdGen)
-randomSeq g 0 = ([], g)
-randomSeq g n = let (r, g') = random g in (\(a, b) -> (r:a, b)) $ randomSeq g' (n-1)
+feedForward :: [Float] -> NN -> [Float]
+feedForward fs [l] = foldr (\(ws, b) a -> (b + (sum $ zipWith (*) fs ws)) : a) [] l
+feedForward fs (l:ls) = feedForward (feedForward fs [l]) ls
 
 main :: IO ()
 main = print $ createNN (mkStdGen 4) [1, 2, 3]
